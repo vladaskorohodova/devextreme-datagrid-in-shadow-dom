422bba195398a172149fff1c5c80c0a0a6d22113
Alyar <>
2024-04-17 03:24:38 +0400

    Add Vue demo

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,22 +1,111 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import "devextreme/dist/css/dx.material.blue.light.css";
+import {
+  DxDataGrid,
+  DxColumn,
+  DxGrouping,
+  DxGroupPanel,
+  DxPager,
+  DxPaging,
+  DxSearchPanel,
+  DxDataGridTypes,
+} from 'devextreme-vue/data-grid';
+import DataSource from 'devextreme/data/data_source';
+import 'devextreme/data/odata/store';
+import DiscountCell from './DiscountCell.vue';
 
-import "devextreme/dist/css/dx.material.blue.light.compact.css";
-import DxButton from "devextreme-vue/button";
-
-const props = defineProps({
-  text: String,
+const dataSource = new DataSource({
+  store: {
+    type: 'odata',
+    version: 2,
+    url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
+    key: 'Id',
+    beforeSend(request) {
+      const year = new Date().getFullYear() - 1;
+      request.params.startDate = `${year}-05-10`;
+      request.params.endDate = `${year}-5-15`;
+    },
+  },
 });
-const count = ref(0);
-const buttonText = computed<String>(
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
-}
+
+const pageSizes = [10, 25, 50, 100];
+
+let collapsed = false;
+
+const onContentReady = (e: DxDataGridTypes.ContentReadyEvent) => {
+  if (!collapsed) {
+    e.component.expandRow(['EnviroCare']);
+    collapsed = true;
+  }
+};
 </script>
+
 <template>
-  <div>
-    <DxButton :text="buttonText" @click="clickHandler"></DxButton>
-  </div>
+  <DxDataGrid
+    :data-source="dataSource"
+    :remote-operations="false"
+    :allow-column-reordering="true"
+    :row-alternation-enabled="true"
+    :show-borders="true"
+    :width="'100%'"
+    @content-ready="onContentReady"
+  >
+    <DxColumn
+      :group-index="0"
+      data-field="Product"
+    />
+    <DxColumn
+      data-field="Amount"
+      caption="Sale Amount"
+      data-type="number"
+      format="currency"
+      alignment="right"
+    />
+    <DxColumn
+      :allow-grouping="false"
+      data-field="Discount"
+      caption="Discount %"
+      data-type="number"
+      format="percent"
+      alignment="right"
+      cell-template="discountCellTemplate"
+      css-class="bullet"
+    />
+    <DxColumn
+      data-field="SaleDate"
+      data-type="date"
+    />
+    <DxColumn
+      data-field="Region"
+      data-type="string"
+    />
+    <DxColumn
+      data-field="Sector"
+      data-type="string"
+    />
+    <DxColumn
+      data-field="Channel"
+      data-type="string"
+    />
+    <DxColumn
+      :width="150"
+      data-field="Customer"
+      data-type="string"
+    />
+
+    <DxGroupPanel :visible="true"/>
+    <DxSearchPanel
+      :visible="true"
+      :highlight-case-sensitive="true"
+    />
+    <DxGrouping :auto-expand-all="false"/>
+    <DxPager
+      :allowed-page-sizes="pageSizes"
+      :show-page-size-selector="true"
+    />
+    <DxPaging :page-size="10"/>
+    <template #discountCellTemplate="{ data: cellData }">
+      <DiscountCell :cell-data="cellData"/>
+    </template>
+  </DxDataGrid>
 </template>
--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -5,7 +5,9 @@ import router from "./router";
 import "./assets/main.css";
 
 const app = createApp(App);
+const shadowHost = document.getElementById("app") as HTMLElement;
+const shadowRoot = shadowHost.attachShadow({ mode: "open" }) as any;
 
 app.use(router);
 
-app.mount("#app");
+app.mount(shadowRoot);
